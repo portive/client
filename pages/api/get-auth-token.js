a1c62bfa25f63f6e387185c0d424f64aa0bb68f2
Sunny Hirai <thesunny@gmail.com>
2022-09-30 14:57:25 -0700

    Make the api endpoint for getting auth token more typical for use in docs

--- pages/api/get-auth-token.js
+++ pages/api/get-auth-token.js
@@ -1,7 +1,8 @@
 import { createAuthToken } from "@portive/auth"
-import { env } from "~/lib/server-env"
 
 export default function handler(req, res) {
-  const authToken = createAuthToken(env.PORTIVE_API_KEY, { expiresIn: "1m" })
+  const authToken = createAuthToken(process.env.PORTIVE_API_KEY, {
+    expiresIn: "1m",
+  })
   res.status(200).json({ authToken })
 }
