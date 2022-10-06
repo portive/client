3051a6e8cab0d6d363425c8dca3b7eb05e423bd3
Sunny Hirai <thesunny@gmail.com>
2022-10-06 14:26:31 -0700

    Update resize to use width/height

--- src/resize/index.ts
+++ src/resize/index.ts
@@ -1,32 +1,39 @@
-type Size = [number, number]
+type ImageSize = { width: number; height: number }
 
-export function resizeIn(size: Size, bounds: Size): Size {
-  if (size[0] <= 0) throw new Error(`width must be greater than 0`)
-  if (size[1] <= 0) throw new Error(`height must be greater than 0`)
-  if (bounds[0] <= 0) throw new Error(`bounds width must be greater than 0`)
-  if (bounds[1] <= 0) throw new Error(`bounds height must be greater than 0`)
+export function resizeIn(size: ImageSize, bounds: ImageSize): ImageSize {
+  if (size.width <= 0) throw new Error(`width must be greater than 0`)
+  if (size.height <= 0) throw new Error(`height must be greater than 0`)
+  if (bounds.width <= 0) throw new Error(`bounds width must be greater than 0`)
+  if (bounds.height <= 0)
+    throw new Error(`bounds height must be greater than 0`)
 
   // if size is smaller than bounds leave it alone
-  if (size[0] < bounds[0] && size[1] < bounds[1]) {
+  if (size.width < bounds.width && size.height < bounds.height) {
     return size
   }
 
-  const aspect = size[0] / size[1]
-  const boundsAspect = bounds[0] / bounds[1]
+  const aspect = size.width / size.height
+  const boundsAspect = bounds.width / bounds.height
 
   if (aspect > boundsAspect) {
     // src is wider than inside so constrain by width
-    return [bounds[0], Math.max(1, Math.round(bounds[0] / aspect))]
+    return {
+      width: bounds.width,
+      height: Math.max(1, Math.round(bounds.width / aspect)),
+    }
   } else {
     // src is taller than inside so constrain by height
-    return [Math.max(1, Math.round(bounds[1] * aspect)), bounds[1]]
+    return {
+      width: Math.max(1, Math.round(bounds.height * aspect)),
+      height: bounds.height,
+    }
   }
 }
 
 const BIG_DIMENSION = 100000
 
-export const resizeInWidth = (size: Size, width: number) =>
-  resizeIn(size, [width, BIG_DIMENSION])
+export const resizeInWidth = (size: ImageSize, width: number) =>
+  resizeIn(size, { width, height: BIG_DIMENSION })
 
-export const resizeInHeight = (size: Size, height: number) =>
-  resizeIn(size, [BIG_DIMENSION, height])
+export const resizeInHeight = (size: ImageSize, height: number) =>
+  resizeIn(size, { width: BIG_DIMENSION, height })
